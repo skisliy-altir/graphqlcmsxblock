c6d0cc6c34ebd7d3ab3727920721ad9e715e1105
andres3210 <andres3210@gmail.com>
2021-06-25 21:13:09 -0500

    GraphQL Query fixes & safe hmtl filter

--- graphqlcmsxblock/graphqlcmsxblock.py
+++ graphqlcmsxblock/graphqlcmsxblock.py
@@ -151,39 +151,6 @@ class GraphQlCmsXBlock(XBlock):
                             tipCssClass
                         }
                     },
-                    table2colMatrix{
-                        ... on table2colMatrix_matrix2col_BlockType{
-                            id,
-                            hasHeaderRow,
-                            table2col{
-                                ... on table2col_BlockType{
-                                    col1, col2
-                                }
-                            }                        
-                        }
-                    },
-                    table3colMatrix{
-                        ... on table3colMatrix_matrix3col_BlockType{
-                            id,
-                            hasHeaderRow,
-                            table3col{
-                                ... on table3col_BlockType{
-                                    col1, col2, col3
-                                }
-                            }
-                        }
-                    },
-                    table4colMatrix{
-                        ... on table4colMatrix_matrix4col_BlockType{
-                            id,
-                            hasHeaderRow,
-                            table4col{
-                                ... on table4col_BlockType{
-                                    col1, col2, col3, col4
-                                }
-                            }
-                        }
-                    },
                     accordionmatrix{
                         ... on accordionmatrix_accordionblock_BlockType{
                             id, accordionblocktitle, accordionblockcontent
@@ -242,48 +209,14 @@ class GraphQlCmsXBlock(XBlock):
                             },
                             tipCssClass
                         }
-                    },
-                    table2colMatrix{
-                        ... on table2colMatrix_matrix2col_BlockType{
-                            id,
-                            hasHeaderRow,
-                            table2col{
-                                ... on table2col_BlockType{
-                                    col1, col2
-                                }
-                            }                        
-                        }
-                    },
-                    table3colMatrix{
-                        ... on table3colMatrix_matrix3col_BlockType{
-                            id,
-                            hasHeaderRow,
-                            table3col{
-                                ... on table3col_BlockType{
-                                    col1, col2, col3
-                                }
-                            }
-                        }
-                    },
-                    table4colMatrix{
-                        ... on table4colMatrix_matrix4col_BlockType{
-                            id,
-                            hasHeaderRow,
-                            table4col{
-                                ... on table4col_BlockType{
-                                    col1, col2, col3, col4
-                                }
-                            }
-                        }
-                    },     
+                    },    
                     accordionmatrix{
                         ... on accordionmatrix_accordionblock_BlockType{
                             id, accordionblocktitle, accordionblockcontent
                         }
                     }                                
                 } 
-            }
-    """
+            }"""
 
     pageGraphQlQuery = """ {
                 slug,
@@ -336,46 +269,13 @@ class GraphQlCmsXBlock(XBlock):
                             tipCssClass
                         }
                     },
-                    table2colMatrix{
-                        ... on table2colMatrix_matrix2col_BlockType{
-                            id,
-                            hasHeaderRow,
-                            table2col{
-                                ... on table2col_BlockType{
-                                    col1, col2
-                                }
-                            }   
-                        }
-                    },
-                    table3colMatrix{
-                        ... on table3colMatrix_matrix3col_BlockType{
-                            id,
-                            hasHeaderRow,
-                            table3col{
-                                ... on table3col_BlockType{
-                                    col1, col2, col3
-                                }
-                            }
-                        }
-                    },
-                    table4colMatrix{
-                        ... on table4colMatrix_matrix4col_BlockType{
-                            id,
-                            hasHeaderRow,
-                            table4col{
-                                ... on table4col_BlockType{
-                                   col1, col2, col3, col4
-                                }
-                            }
-                        }
-                    },
                     accordionmatrix{
                         ... on accordionmatrix_accordionblock_BlockType{
                             id, accordionblocktitle, accordionblockcontent
                         }
                     }                                        
                 } 
-            } """
+            }"""
 
 
     # Fields are defined on the class.  You can access them in your code as
@@ -570,7 +470,7 @@ class GraphQlCmsXBlock(XBlock):
             resp = requests.post(self.cmsApi, json={
                 "query": "query MyQuery {entries(section: \"pages\", slug: \"" + self.entrySlug + "\" limit: 1) " + self.pageGraphQlQuery + " }"
             })
-
+        
         entry = resp.json()['data']['entries'][0]
         title = entry['title']
         
